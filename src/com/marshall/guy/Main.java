a527b603715ad50c0845b9813c658350b4847c0b
Guy Marshall <guymarshall9@gmail.com>
2021-11-25 19:36:25 +0000

    Initial commit

---
+++ src/com/marshall/guy/Main.java
@@ -0,0 +1,57 @@
+package com.marshall.guy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int gcd(int number1, int number2) {
+        if (number2 == 0) {
+            return number1;
+        } else {
+            return gcd(number2, number1 % number2);
+        }
+    }
+
+    public static void main(String[] args) {
+	    int numberTriplesCalculated = 0;
+        int sideA = 1;
+
+        System.out.print("Number of triples to calculate: ");
+        Scanner scanner = new Scanner(System.in);
+        int numberTriplesNeeded = scanner.nextInt();
+
+        boolean loopA = numberTriplesNeeded > 0;
+
+        int[] result = {3};
+
+        while (loopA) {
+            int sideB = 1;
+            boolean loopB = true;
+            while (sideB <= sideA && loopB) {
+                if (Math.round(gcd(sideA, sideB)) == 1) {
+                    int sideC = (int)Math.round(Math.hypot(sideA, sideB));
+
+                    if (sideC * sideC == (sideA * sideA + sideB * sideB)) {
+                        numberTriplesCalculated++;
+                        result = new int[]{sideB, sideA, sideC};
+                        System.out.println(Arrays.toString(result));
+
+                        if (numberTriplesCalculated == numberTriplesNeeded) {
+                            loopA = false;
+                            loopB = false;
+                        }
+                    }
+                }
+
+                sideB++;
+            }
+
+            sideA++;
+        }
+
+        if (numberTriplesNeeded < 0) {
+            System.out.printf("%d is invalid", numberTriplesNeeded);
+        }
+    }
+}
